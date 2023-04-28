0236e2d7d7fbd257ef73b93b081e5e98d520b3df
02Alexis <m_chael1012@hotmail.com>
2023-04-28 17:12:29 -0500

    dando nuevos estilos

--- src/app/index.js
+++ src/app/index.js
@@ -1,2 +1,24 @@
-import '../index.html'
+// import '../index.html'
 import '../styles/indexStyles/styles.css'
+
+document.getElementById("btn_menu").addEventListener("click", mostrar_menu);
+
+document.getElementById("back_menu").addEventListener("click", ocultar_menu);
+
+document.getElementById("btn_close").addEventListener("click", ocultar_menu);
+
+
+    const nav = document.getElementById("nav");
+    let background_menu = document.getElementById("btn_close");
+
+    // nav = document.getElementById("nav");
+    background_menu = document.getElementById("back_menu");
+
+    function mostrar_menu(){
+    nav.style.right = "0px";
+    background_menu.style.display = "block";
+    }
+    function ocultar_menu(){
+    nav.style.right = "-450px";
+    background_menu.style.display = "none";
+    }
